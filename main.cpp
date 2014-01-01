c2697614fe8817d4dceb6fcf4844dd23932aab40
Victor Demin <mail@vdemin.com>
2014-01-01 18:32:56 +0400

    Init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Victor
+ *
+ * Created on January 1, 2014, 6:25 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
