fb4247203077683be7421ff617a5aee3c0f76ae0
Victor Demin <mail@vdemin.com>
2014-01-01 22:44:05 +0400

    fix bug in nmod_mat_print_pretty;

--- main.cpp
+++ main.cpp
@@ -11,16 +11,10 @@
  * 
  */
 int main(int argc, char** argv) {
-    int n = 10;
     mat_t S1(5, 10, 5);
     frandxx state;
     S1.set_randtest(state);
-    std::cout << S1.at(1,1) << std::endl;
     print_mat(S1);
-
-    flint_printf("S1:\n");
-    print_pretty(S1);
-    flint_printf("\n\n");
 
     return 0;
 }
--- tools.cpp
+++ tools.cpp
@@ -1,15 +1,35 @@
 #include "main.h"
 
+/**
+ * Print matrix
+ * (fix bug in nmod_mat_print_pretty)
+ * @param obj
+ */
 void print_mat(const mat_t& obj)
 {
-//    nmod_mat_print_pretty(obj._mat());
-//    return;
-    for (int i = 0; i < obj.rows(); i++)
+    slong i, j;
+    int width;
+    char fmt[FLINT_BITS + 5];
+
+    flint_printf("<%wd x %wd integer matrix mod %wu>\n", obj.rows(), obj.cols(), obj.modulus());
+
+    if (!(obj.cols()) || !(obj.rows()))
+        return;
+
+    width = n_sizeinbase(obj.modulus(), 10);
+
+    sprintf(fmt, "%%%dlu", width);
+    for (i = 0; i < obj.rows(); i++)
     {
-        for (int j = 0; j < obj.cols(); j++)
+        printf("[");
+
+        for (j = 0; j < obj.cols(); j++)
         {
-            cout << obj.at(i, j)._limb() << " ";
+            printf(fmt, obj.at(i, j)._limb());
+            if (j + 1 < obj.cols())
+                printf(" ");
         }
-        cout << endl;
+
+        printf("]\n");
     }
 }
