d776fe2b8c69148f625898760a7dc7f27236f7ce
Victor Demin <mail@vdemin.com>
2014-01-03 03:21:08 +0400

    conf settings

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -53,7 +53,7 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=-L/usr/local/lib -Wl,-rpath,/usr/local/lib -lflint -lmpfr -lgmp -lpthread
+LDLIBSOPTIONS=-Llib -Wl,-rpath,share -lflint -lmpfr -lgmp -lpthread
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
@@ -66,12 +66,12 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ghw: ${OBJECTFILES}
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -g -I/usr/local/include/flint -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -g -Iinclude -Iinclude/flint -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
 
 ${OBJECTDIR}/tools.o: tools.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -g -I/usr/local/include/flint -MMD -MP -MF $@.d -o ${OBJECTDIR}/tools.o tools.cpp
+	$(COMPILE.cc) -g -Iinclude -Iinclude/flint -MMD -MP -MF $@.d -o ${OBJECTDIR}/tools.o tools.cpp
 
 # Subprojects
 .build-subprojects:
