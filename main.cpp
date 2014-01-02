41ce3063f29e5a44f8907f03f5db312cce46b62e
Victor Demin <mail@vdemin.com>
2014-01-02 21:49:07 +0400

    clean code

--- main.cpp
+++ main.cpp
@@ -17,45 +17,6 @@ int main(int argc, char** argv) {
     print_mat(S1);
     cout << "Matrix rank: " << S1.rank() << endl;
 
-//    slong rank = nullspace(S1).get<0>();
-//    flint::nmod_matxx_expression<
-//        flint::operations::ltuple_get_op<1u>,
-//        flint::tuple<
-//            flint::ltuple_expression<
-//                flint::tuple<
-//                    long int,
-//                    flint::tuple<
-//                        //nmod_matxx
-//                        flint::nmod_matxx_expression<
-//                            flint::operations::immediate,
-//                            flint::detail::nmod_mat_data
-//                        >,
-//                        flint::empty_tuple
-//                    >
-//                >,
-//                flint::operations::nullspace_op,
-//                flint::tuple<
-//                    //nmod_matxx
-//                    const flint::nmod_matxx_expression<
-//                        flint::operations::immediate,
-//                        flint::detail::nmod_mat_data
-//                    >&,
-//                    flint::empty_tuple
-//                >
-//            >,
-//            flint::empty_tuple
-//        >
-//    >
-//        obj  = nullspace(S1).get<1>();
-    /*
-
-    slong rank;
-    nmod_matxx S2;
-    ltupleref(rank, S2) = nullspace(S1);
-    cout << "Matrix rank: " << rank << endl;
-    print_mat(S2);
-     * */
-
     nmod_mat_t X;
     nmod_mat_init(X, S1.cols(), S1.cols(), S1.modulus());
     slong rank;
