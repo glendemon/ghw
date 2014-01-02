4ddec235d0007be0a29af61e945bd282e400f653
Victor Demin <mail@vdemin.com>
2014-01-03 03:23:44 +0400

    Release as default;

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,14 +6,6 @@
 CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
-# Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
-CND_ARTIFACT_NAME_Debug=ghw
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ghw
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Debug=ghw.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ghw.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
@@ -22,6 +14,14 @@ CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/ghw
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=ghw.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/ghw.tar
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=ghw
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ghw
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=ghw.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ghw.tar
 #
 # include compiler specific variables
 #
